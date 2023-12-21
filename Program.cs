647cf7e35a40e69234dc63763f93ea3be1f3fe11
fazliddinxdev <fazliddinxdeveloper@gmail.com>
2023-12-21 18:40:21 +0500

    Task 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Basic ketma-ketlik mavzusi topshiriqlari
+// 1 topshiriq
+
+const double PI = 3.14f;
+Console.Write("Enter radius: ");
+string inputRadiusValue = Console.ReadLine();
+double radius = Convert.ToDouble(inputRadiusValue);
+Console.WriteLine("Calculating ... ");
+Console.WriteLine($"Doira yuzasi {PI * (Math.Pow(radius, 2))}");
+Console.WriteLine($"Aylana uzunligi {2* PI * radius}");
+
+
+// 2-masala
